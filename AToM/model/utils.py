0e0ca091af9cc3941f0c8a146f949fd239270667
Yang Wang <yangwangx@gmail.com>
2024-04-12 14:59:46 -0700

    Update utils.py
    Annotate dimension information related to PositionEncoding module.

--- AToM/model/utils.py
+++ AToM/model/utils.py
@@ -15,19 +15,28 @@ class PositionalEncoding(nn.Module):
 
         self.dropout = nn.Dropout(p=dropout)
 
+        # pe: shape [max_len, d_model]
+        # max_len is set to a large number, e.g. 500
+        # d_model is the position embedding dimension, e.g., 1024
         pe = torch.zeros(max_len, d_model)
+        # position: shape [max_len, 1], value [0, 1, ..., max_len - 1]
         position = torch.arange(0, max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-np.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
+
+        # pe: shape [max_len, 1, d_model]
         pe = pe.unsqueeze(0).transpose(0, 1)
 
         self.register_buffer("pe", pe)
 
     def forward(self, x):
+        # The last dimension of x must match `d_model`
         if self.batch_first:
+            # x: [B, T, d_model]
             x = x + self.pe.permute(1, 0, 2)[:, : x.shape[1], :]
         else:
+            # x: [T, B, d_model]
             x = x + self.pe[: x.shape[0], :]
         return self.dropout(x)
 
